7e99b6b15ee6bebf19f1d3b88ec84507d07f28af
courtneyc148 <courtneyc148@gmail.com>
2020-10-18 20:06:06 -0400

    js homework

---
+++ app.js
@@ -0,0 +1,63 @@
+let variableOne = 10;
+let variableTwo = 5;
+
+// addition
+function sum (x, y) {
+        return x + y;
+}
+
+console.log(sum(variableOne, variableTwo));
+
+
+// subtraction
+function takeAway (a, b) {
+    return a - b;
+}
+
+console.log(takeAway(variableOne, variableTwo));
+
+
+// multiplication
+function multiply (c, d) {
+    return c * d;
+}
+
+console.log(multiply(variableOne, variableTwo));
+
+
+// Division
+function division (e, f) {
+    return e / f;
+}
+
+console.log(division(variableOne, variableTwo));
+
+
+
+// Arrow Function
+
+
+// addition 
+ let sum = (x, y) =>  x + y;
+
+
+console.log(sum(variableOne, variableTwo));
+
+// addition 
+let multiply = (x, y) =>  x * y;
+
+
+console.log(multiply(variableOne, variableTwo));
+
+// addition 
+let division = (x, y) =>  x / y;
+
+
+console.log(division(variableOne, variableTwo));
+
+// addition 
+let subtraction = (x, y) =>  x - y;
+
+
+console.log(subtraction(variableOne, variableTwo));
+
